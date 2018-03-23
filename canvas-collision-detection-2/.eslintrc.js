dfcfadcda114f9c7a85d5a1285f3011edc01b3c4
Keith Rodriguez <rodriguez.keith@gmail.com>
2018-03-23 01:06:49 -0400

    Collision detection part 2

---
+++ canvas-collision-detection-2/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+        sourceType: 'module'
+    },
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single']
+    },
+    globals: {
+        module: true,
+        require: true,
+        __dirname: true
+    }
+}
